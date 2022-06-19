855daf7b56a5809f6e9131c0340b98f6d4d3b63e
Yusuf-oluwaseyi <85449521+Yusuf-oluwaseyi@users.noreply.github.com>
2022-06-19 22:55:59 +0100

    update

---
+++ lesson1.py
@@ -0,0 +1,11 @@
+print("This is a new line")
+# comments
+# variables in python.
+myint = 70
+myint2 = 40
+result = myint + myint2
+print(result)
+firstName = "Omotola"
+lastName = "Adejimi"
+fullName = firstName + " " + lastName
+print(fullName)
