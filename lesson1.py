6d5f73cd1e7df2596928f219d99fc9cf01f3fc80
Yusuf-oluwaseyi <85449521+Yusuf-oluwaseyi@users.noreply.github.com>
2022-06-19 23:43:14 +0100

    Beginner

--- lesson1.py
+++ lesson1.py
@@ -1,11 +1,27 @@
-print("This is a new line")
+#print("This is a new line")
 # comments
 # variables in python.
-myint = 70
-myint2 = 40
-result = myint + myint2
-print(result)
-firstName = "Omotola"
-lastName = "Adejimi"
-fullName = firstName + " " + lastName
-print(fullName)
+# myint = 70
+# myint2 = 40
+# result = myint + myint2
+# print(result)
+# firstName = "Omotola"
+# lastName = "Adejimi"
+# fullName = firstName + " " + lastName
+# print(fullName)
+
+# lesson 1
+import string
+from xmlrpc.client import boolean
+
+
+print("Hello World")
+
+# lesson 2 
+# variales in python
+int = -34
+float = 8.4
+string = "Adejimi"
+# boolean = true or false
+
+print(float)
