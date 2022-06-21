9af4f61b541bb341ab52214d1303014559e568b2
Yusuf-oluwaseyi <85449521+Yusuf-oluwaseyi@users.noreply.github.com>
2022-06-21 23:57:27 +0100

    Exercise

--- lesson1.py
+++ lesson1.py
@@ -25,3 +25,16 @@ string = "Adejimi"
 # boolean = true or false
 
 print(float)
+
+# change this code
+mystring = "hello"
+myfloat = 10.0
+myint = 20
+
+# testing code
+if mystring == "hello":
+    print("String: %s" % mystring)
+if isinstance(myfloat, float) and myfloat == 10.0:
+    print("Float: %f" % myfloat)
+if isinstance(myint, int) and myint == 20:
+    print("Integer: %d" % myint)
